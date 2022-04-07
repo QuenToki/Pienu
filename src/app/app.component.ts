7fd9e14c2967fe50aa667d3fe5513b3c454c590c
Kevin Vanphavong <kevin45van@hotmail.fr>
2022-04-07 13:03:17 +0200

    delete unused code for the side menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,6 @@ import { ModalController } from '@ionic/angular';
 import { SplashComponent } from './splash/splash.component';
 
 import { Platform } from '@ionic/angular';  
-import { SplashScreen } from '@ionic-native/splash-screen';  
-import { StatusBar } from '@ionic-native/status-bar'; 
 
 @Component({
   selector: 'app-root',
@@ -17,51 +15,16 @@ export class AppComponent {
   constructor(   
     private modalController : ModalController,
     private platform: Platform,  
-    // private splashScreen: typeof SplashScreen,  
-    // private statusBar: typeof StatusBar  
   ) {   
     this.presentModal();  
-    this.sideMenu();  
     this.initializeApp();  
   }
 
   initializeApp() {  
     this.platform.ready().then(() => {  
-      // this.statusBar.styleDefault();   
-      // this.splashScreen.hide();  
     });  
   } 
 
-  sideMenu() {  
-    // this.navigate =   
-    // [  
-    //     { 
-    //     title : 'App',
-    //     url   : '/apps',
-    //     icon  : 'apps' 
-    //     },
-    //   { 
-    //     title : 'Book',  
-    //     url   : '/book',  
-    //     icon  : 'book'  
-    //   },   
-    //   {  
-    //     title : 'Paint',  
-    //     url   : '/paint',  
-    //     icon  : 'brush'   
-    //   },  
-    //   {  
-    //     title : 'Contacts',  
-    //     url   : '/contacts',  
-    //     icon  : 'contacts'  
-    //   },   
-    //   {
-    //       title : 'Facebook',
-    //       url   : '/facebook.com',
-    //       icon  : 'logo-facebook'
-    //   },
-    // ]; 
-  }  
  
   async presentModal() {
     const modal = await this.modalController.create({
