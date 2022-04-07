e5365677b1928ef5c310adb13edd3ff8c6aaf040
Romain Carvalho <romaincarvalho@MacBook-Pro-de-Romain.local>
2022-04-08 00:08:34 +0200

    connexion api

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,60 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
+  loginData = {
+    mail: '',
+    password: ''
+  };
+  recupInfo = {
+    mail: '',
+    password: ''
+  };
+  erreur ={
+    message:""
+  };
+  constructor(private router: Router,) { 
+    var token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNjQ5MzQwNjA5LCJleHAiOjE2NDk0MjcwMDl9.tunJpyyAS6VubhB9YT78j0kZx_QbFFU_dYOpvJSuOgA';
 
-  constructor() { }
+  }
+  
+  login() {
+    fetch('https://pienu.herokuapp.com/api/auth/signin',{
+      method: 'POST',
+      body: JSON.stringify(this.loginData),
+      headers: {
+        "Content-Type": "application/json",
+      }
+    }).
+    then(response => {
+        return response.json();
+    })
+    .then(data => {
+      console.log(data);
+      console.log(data.accessToken);
+      console.log(data.message);
+      if(data.accessToken !== undefined){
+        console.log("token : "+data.accessToken);
+        localStorage.setItem('token', data.accessToken);
+        this.router.navigateByUrl('/welcome');
+      }else{
+        this.erreur.message = data.message;
 
-  ngOnInit() {
+      }
+
+    });
+    
+    
+    
+   /* if(this.loginData.mail === this.recupInfo.mail && this.loginData.password === this.recupInfo.password){
+      console.log('ok');
+      
+      
+    }else{
+      this.erreur.message = "Erreur d'authentification";
+    }*/
+  }
+  
+  ngOnInit(): void {
   }
 
 }
